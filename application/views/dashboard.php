a1f8a1e0b27bf4cbbb8816ed0f6397ee32467c34
naelhanafi <naelhanafi.kuliah@gmail.com>
2024-06-25 21:13:29 +0700

    rafinfutsal

---
+++ application/views/dashboard.php
@@ -0,0 +1,58 @@
+
+<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="<?= base_url() ?>assets/images/lpp.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        <h1 style="color: red;"><strong> Selamat Datang di "RAFIN" Futsal</strong></h1>
+          <h3><strong style="color: white;">Menyediakan Penyewaan lapangan futsal</strong></h3>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?= base_url() ?>assets/images/lpp01.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        <h1 style="color: red;"><strong>-Tempat Yang Nyaman, Terjangkau segala kalangan-</strong></h1>
+        <h3><strong style="color: white;">--Untuk Latihan Atau Ke Jenjang Professional Tim Futsal Bekasi--</strong></h3>
+        <p></p>
+        </div>
+      </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<br><br><br>
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <h2 class="section-title-underline style-2">
+            <span style="color: black;">Tentang RAFIN Futsal</span>
+            </h2>
+          </div>
+          <div class="col-lg-8">
+          <p class="lead">RAFIN Futsal adalah perusahaan penyedia jasa penyewaan lapangan futsal, Di dirikan pada tahun 2024 oleh pemiliknya yang bernama RAFIN.</p>
+            <p>RAFIN Futsal sendiri terletak di tengah Kota Bekasi, Tepat nya di Jl.Kaliabang Tengah, Bekasi Utara, Kec.Babelan, Kab.Bekasi, RAFIN Futsal sendiri sudah di lengkapi fasilitas-fasilitas seperti Toilet yang bersih, Parkiran, Mushola, Warung dan WI-FI sehingga membuat pengunjung merasa lebih nyaman.</p>
+          </div>
+              </div>
+            </div>
+          </div>
+        
+
+
+
+<br><br><br><br>
+
+
+  </div>
+</div>
